068fff15bbd62ba892a1052cd85534075a321dee
ItCGM <reinaldofs@PREFEITURA.SP.GOV.BR>
2024-10-09 15:37:37 -0300

    feat(p1-pdmn): cria componente principal e faz hello

---
+++ p1-pdmn/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="container text-center">
+      <div className="row">
+        <div className="col">
+          <h1>Hello, tarefas</h1>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
