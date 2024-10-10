9d02e707485626b75094b1e92646b7d03564b1f5
ItCGM <reinaldofs@PREFEITURA.SP.GOV.BR>
2024-10-10 13:46:44 -0300

    feat(p1-pdmn): cria componente de entrada

--- p1-pdmn/src/App.js
+++ p1-pdmn/src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import TarefaEntrada from './TarefaEntrada';
 
 function App() {
   return (
-    <div className="container text-center">
-      <div className="row">
-        <div className="col">
-          <h1>Hello, tarefas</h1>
+    <div className='container'>
+      <div className='row'>
+        <div className='col-lg-6 offset-lg-3'>
+          <TarefaEntrada />
         </div>
       </div>
     </div>
---
+++ p1-pdmn/src/TarefaEntrada.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const TarefaEntrada = () => {
+
+    return(
+        <div className='container'>
+        <form>
+          <div className='row mb-3'>
+            <div className='col'>
+              <input type='text' className='form-control text-center' placeholder='Digite a descrição da tarefa'/>
+            </div>
+          </div>
+          <div className='row mb-3'>
+            <div className='col'>
+              <div className='d-grid gap-2'>
+                <button className='btn btn-primary' type='submit'>Ok</button>
+              </div>
+            </div>
+          </div>
+        </form>
+      </div>
+    )
+};
+export default TarefaEntrada;
